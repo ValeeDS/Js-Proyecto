61880468ec7493ad626daa2f543fd8476d4f558b
Valeria Del Savio <valeria.delsavio@hotmail.com>
2022-02-07 00:36:52 -0300

    functions added

--- js/main.js
+++ js/main.js
@@ -1,21 +1,85 @@
-const listaCategorias = ['PANIFICADOS', 'LACTEOS', 'POSTRES'];
-
-//DECLARACION DE CLASE PRODUCTO
-class Producto {
-        constructor(id, nombre, precio,categoria) {
-                this.id = parseInt(id);
-                this.nombre = nombre;
-                this.precio = parseFloat(precio);
-                //Agrego la propiedad categorias
-                this.categoria = categoria;
-        }
+class Task {
+    constructor(task, status) {
+        this.task = task;
+        this.status = false;
+    }
 }
 
-//DECLARAMOS UN ARRAY VACIO
-const productos = [];
-//CON PUSH, AGREGAMOS OBJETOS INSTANCIADOS AL ARRAY
-productos.push(new Producto(1, "PAN", 50,categorias[0]));
-productos.push(new Producto(2, "PASTAFROLA", 150,categorias[0]));
-productos.push(new Producto(3, "LECHE", 200,categorias[1]));
-productos.push(new Producto(4, "FLAN", 170,categorias[2]));
-console.log(productos);
+class Proyect {
+    constructor(name, description, start_date, due_date, tasks, status){
+        this.name = name;
+        this.description = description;
+        this.start_date = new Date(start_date);
+        this.due_date = new Date(due_date);
+        this.tasks = tasks; // Type Array
+        this.status = false;
+    }
+}
+
+function create_proyect(name, description, start_date, due_date){
+    proyecto = new Proyect(name, description, start_date, due_date);
+
+    let tasks = arrayTasks();
+    proyecto.tasks = tasks;
+
+    return proyecto;
+}
+
+function create_task(task){
+    task = new Task(task);
+
+    return task;
+}
+
+function arrayTasks(){
+    const tasks = [];
+    let newTask;
+    
+    do {
+        newTask = prompt("¿Desea ingresar una nueva tarea? Ingrese Y o N?");
+        if (newTask != null && newTask == "N") {
+            break;
+        }
+        else if (newTask != "Y"){
+            alert("Por favor ingrese Y o N");
+        }
+        else {
+            let task = prompt("Ingrese la tarea");
+
+            tasks.push(newTask = create_task(task));
+        }
+    }
+    while(true);
+
+    return tasks;
+}
+
+function Add_proyects(){
+    const proyects = [];
+    let newProy;
+
+    do {
+        newproy = prompt("¿Desea ingresar un nuevo proyecto? Ingrese Y o N?");
+        if (newproy != null && newproy == "N") {
+            break;
+        }
+        else if (newproy != "Y"){
+            alert("Por favor ingrese Y o N");
+        }
+        else {
+            let name = prompt("Ingrese el nombre del Proyecto");
+            let description = prompt("Ingrese una breve descripción del proyecto");
+            let start_date = prompt('Ingrese la fecha de inicio con el siguiente formato: "Mes Fecha, Año"');
+            let due_date = prompt('Ingrese la fecha límite con el siguiente formato: "Mes Fecha, Año"');
+
+            proyects.push(newProy = create_proyect(name, description, start_date, due_date));
+        }
+    }
+
+    while(true);
+
+    return proyects;
+}
+
+const proyectos = Add_proyects();
+console.log(proyectos);
