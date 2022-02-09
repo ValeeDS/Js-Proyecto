b0f51425da27da773326ebf4d5cb67ae022551eb
Valeria Del Savio <valeria.delsavio@hotmail.com>
2022-02-08 23:12:24 -0300

    DOM added

--- js/main.js
+++ js/main.js
@@ -1,26 +1,10 @@
-class Task {
-    constructor(task, status) {
-        this.task = task;
-        this.status = false;
-    }
-}
-
-class Proyect {
-    constructor(name, description, start_date, due_date, tasks, status){
-        this.name = name;
-        this.description = description;
-        this.start_date = new Date(start_date[0],start_date[1],start_date[2]);
-        this.due_date = new Date(due_date[0],due_date[1],due_date[2]);
-        this.tasks = tasks; // Type Array
-        this.status = false;
-    }
-}
-
 function create_proyect(name, description, start_date, due_date){
     proyecto = new Proyect(name, description, start_date, due_date);
 
     let tasks = arrayTasks();
     proyecto.tasks = tasks;
+
+    show_frontend(proyecto);
 
     return proyecto;
 }
@@ -71,12 +55,12 @@ function Add_proyects(){
             let description = prompt("Ingrese una breve descripción del proyecto");
             let start_date = [
                 parseInt(prompt("Ingrese el año de la fecha de inicio")),
-                parseInt(prompt("Ingrese el mes de la fecha de inicio (número)"))-1,
+                parseInt(prompt("Ingrese el mes de la fecha de inicio (número)")),
                 parseInt(prompt("Ingrese el día de la fecha de inicio")),
                 ];
             let due_date = [
                 parseInt(prompt("Ingrese el año de la fecha límite")),
-                parseInt(prompt("Ingrese el mes de la fecha límite (número)"))-1,
+                parseInt(prompt("Ingrese el mes de la fecha límite (número)")),
                 parseInt(prompt("Ingrese el día de la fecha límite")),
                 ];
 
@@ -89,6 +73,31 @@ function Add_proyects(){
     return proyects;
 }
 
+function show_frontend(proyecto){
+    let proy_div = document.createElement("div");
+    proy_div.id = `${proyecto.name}`;
+    proy_div.class = "proyect";
+
+    proy_div.innerHTML = `<h3>Proyecto: ${proyecto.name}</h3>
+                         <p>${proyecto.description}</p>
+                         <p>Desde ${proyecto.start_date} hasta ${proyecto.due_date}</p>
+                         <ul id="${proyecto.name}_tasks">Tareas:</ul>
+                         <br>`;
+
+    document.body.appendChild(proy_div);
+
+    tasks_ul = document.getElementById(`${proyecto.name}_tasks`);
+
+    for (const task of proyecto.tasks){
+        let li = document.createElement("li");
+        li.innerHTML = task.task;
+        tasks_ul.appendChild(li);
+    }
+
+    /*proy_div.innerHTML = `<\ul>`;
+    document.body.appendChild(proy_div);*/
+}
+
 function filtrar_completado(array,stat){
     let result = [];
 
@@ -97,11 +106,23 @@ function filtrar_completado(array,stat){
     return result
 }
 
+class Task {
+    constructor(task, status) {
+        this.task = task;
+        this.status = false;
+    }
+}
+
+class Proyect {
+    constructor(name, description, start_date, due_date, tasks, status){
+        this.name = name;
+        this.description = description;
+        this.start_date = new Date(start_date[0],start_date[1]-1,start_date[2]);
+        this.due_date = new Date(due_date[0],due_date[1]-1,due_date[2]);
+        this.tasks = tasks; // Type Array
+        this.status = false;
+    }
+}
+
 const proyectos = Add_proyects();
 console.log(proyectos);
-
-const proy_completos = filtrar_completado(proyectos,true);
-console.log(proy_completos);
-
-const proy_pendientes = filtrar_completado(proyectos,false);
-console.log(proy_pendientes);
